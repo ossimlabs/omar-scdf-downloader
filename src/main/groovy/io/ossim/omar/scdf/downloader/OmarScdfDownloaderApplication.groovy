90adb298a5d845ec7b020508fafc4c61f41670bb
Chris Dowin <cdowin@radiantblue.com>
2017-06-08 10:58:38 -0400

    Groovified code, changed configs, constructor, commenting

--- src/main/groovy/io/ossim/omar/scdf/downloader/OmarScdfDownloaderApplication.groovy
+++ src/main/groovy/io/ossim/omar/scdf/downloader/OmarScdfDownloaderApplication.groovy
@@ -1,6 +1,5 @@
 package io.ossim.omar.scdf.downloader
 
-import groovy.json.JsonOutput
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
@@ -12,22 +11,16 @@ import org.springframework.messaging.Message
 import org.springframework.messaging.handler.annotation.SendTo
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.core.io.ResourceLoader
-import org.springframework.core.io.Resource
 import groovy.json.JsonSlurper
 import groovy.json.JsonBuilder
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import com.amazonaws.AmazonClientException;
-import com.amazonaws.AmazonServiceException;
-import com.amazonaws.auth.profile.ProfileCredentialsProvider;
-import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata
 import com.amazonaws.services.s3.model.GetObjectRequest
 
 /**
- * Created by adrake on 5/31/2017
+ * Created by  on 5/31/2017
  */
 
 @SpringBootApplication
@@ -37,22 +30,37 @@ class OmarScdfDownloaderApplication {
 	/**
 	 * The application logger
 	 */
-	private Logger logger = LoggerFactory.getLogger(this.getClass())
+	private final Logger logger = LoggerFactory.getLogger(this.getClass())
 
 	/**
 	 * Filepath passed in from application.properties
 	 */
 	@Value('${filepath}')
-	String filepath
+	final String filepath
 
+	/**
+	 * AWS access key
+	 */
 	@Value('${cloud.aws.credentials.accessKey')
-	String accessKey
+	final String accessKey
 
+	/**
+	 * AWS secret key
+	 */
 	@Value('${cloud.aws.credentials.secretKey')
-	String secretKey
+	final String secretKey
 
-	AmazonS3Client s3Client = new AmazonS3Client(accessKey, secretKey)
+	/**
+	 * The client used to connect to S3 for downloading files
+	 */
+	final AmazonS3Client s3Client
 
+	/**
+	 * Constructor
+	 */
+	OmarScdfDownloaderApplication(){
+		s3Client = new AmazonS3Client(accessKey, secretKey)
+	}
 
 	/**
 	 * ResouceLoader used to access the s3 bucket objects
@@ -78,46 +86,50 @@ class OmarScdfDownloaderApplication {
 	 * Receives a message from the SCDF aggregator, downloads the files in the message
 	 * and puts them in the filepath on the SCDF server
 	 * @param message The message object from the SCDF aggregrator (in JSON)
-	 * @return a JSON message of the files, and bucket that need to be downloaded
+	 * @return a JSON message of the files downloaded
 	 */
 	@StreamListener(Processor.INPUT) @SendTo(Processor.OUTPUT)
-	String download(Message<?> message){
+	final String download(final Message<?> message){
 
 		if(logger.isDebugEnabled()){
 			logger.debug("Message received: ${message}")
 		}
 
 		final def parsedJson = new JsonSlurper().parseText(message.payload)
+
+		// The list of files successfully downloaded
+		final ArrayList<String> filesDownloaded = new ArrayList<String>()
+
+		// Local storage vars for the json iteration
 		String s3Bucket
 		String s3Filename
-		String files = new String [parsedJson.files];
+		File localFile
+		ObjectMetadata object
 
 		// Loop through each received JSON file and download
-		for(int i = 0; i < parsedJson.files; i++) {
-			s3Bucket = parsedJson.files[i].bucket
-			s3Filename = parsedJson.files[i].filename
-			files[i] = s3Filename
-			File localFile = new File(filepath+s3Filename);
-			ObjectMetadata object = s3Client.getObject(new GetObjectRequest(s3Bucket, s3Filename), localFile);
+		parsedJson.files.each { file ->
 
+			s3Bucket = file.bucket
+			s3Filename = file.filename
+
+			// Create the file handle
+			localFile = new File(filepath+s3Filename)
+
+			// Download the file from S3
+			object = s3Client.getObject(new GetObjectRequest(s3Bucket, s3Filename), localFile);
+
+			// Add the file to the list of successful downloads
+			filesDownloaded = s3Filename
 		}
 
-    //    JsonBuilder filesDownloaded
+        JsonBuilder filesDownloadedJson = new JsonBuilder()
 
-		// Create a new instance of Gson
-//		Gson filesDownloaded = new Gson();
+		filesDownloadedJson(files: filesDownloaded)
 
 		if(logger.isDebugEnabled()){
-			logger.debug("\n-- files: ${files} \n")
+			logger.debug("filesDownloadedJson: ${filesDownloadedJson}")
 		}
 
-//		String filesDownloadedString = filesDownloaded.toJson(days);
-
-
-
-		if(logger.isDebugEnabled()){
-			logger.debug("filesDownloaded: ${filesDownloaded}")
-		}
-		return filesDownloadedString()
+		return filesDownloadedJson.toString()
 	}
 }
