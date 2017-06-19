d588b1e35325886419eb3e3d76d1e1641acd3653
slallier <stephen.lallier@digitalglobe.com>
2017-06-19 19:52:54 -0400

    added uuid

--- src/main/groovy/io/ossim/omar/scdf/downloader/OmarScdfDownloaderApplication.groovy
+++ src/main/groovy/io/ossim/omar/scdf/downloader/OmarScdfDownloaderApplication.groovy
@@ -91,7 +91,8 @@ class OmarScdfDownloaderApplication
 			String month = timeStamp.substring(4,6)
 			String day = timeStamp.substring(6,8)
 			String hour = timeStamp.substring(8,10)
-			String directories = filepath + "/" + year + "/" + month + "/" + day + "/" + hour + "/"
+			String UUID = UUID.randomUUID().toString()
+			String directories = filepath + "/" + year + "/" + month + "/" + day + "/" + hour + "/" + UUID + "/"
 			if (Files.notExists(Paths.get(directories)))
 			{
 				Files.createDirectories(Paths.get(directories))
